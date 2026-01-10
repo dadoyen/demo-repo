c628b6d0408ae364a833482158770d0d1f62cbce
dadoyen2 <opecynow@gmail.com>
2026-01-10 12:17:28 +0100

    some changes

--- Accountoperation.py
+++ Accountoperation.py
@@ -1,22 +1,22 @@
 accountName = "Ope"
 accountBalance = 100
 accountPassword = "blessing"
-while true:
-    operation = print("Hi, hit on any of the buttons below and lets get started")
 
-    print("press b to get the balance")
-    print("press d to make a deposit")
-    print("press w for withdrawer")
-    print("press s to show the account balance")
-    print("press q to quit")
-    print(" ")
+operation = print("Hi, hit on any of the buttons below and lets get started")
+
+print("press b to get the balance")
+print("press d to make a deposit")
+print("press w for withdrawer")
+print("press s to show the account balance")
+print("press q to quit")
+print(" ")
     
-    action = input("welcome, what do you want to do")
-    action = action.lower()
-    action = action(0)
-    print(" ")
+action = input("welcome, what do you want to do")
+action = action.lower()
+action = action(0)
+print(" ")
 
-    if action == "b":
+if action == "b":
         print("get balance")
         userpassword = input("please enter the account password")
         if userpassword != accountPassword:
@@ -24,7 +24,7 @@ while true:
         else:
             print("your account balace is: ", accountBalance)
 
-    elif action == "s":
+elif action == "s":
         print("show the account details")
         userpassword = input("please enter the account password")
         if userpassword != accountPassword:
@@ -34,12 +34,12 @@ while true:
             print("account balance: ", accountBalance)
             print("account password: ", accountPassword)
     
-    elif action == "d":
+elif action == "d":
         print("make a deposit")
-        amountToDepsit = input("enter the amount to deposit")
+        amountToDeposit = input("enter the amount to deposit")
         accountBalance += amountToDeposit #accountBalance = accountBalance+amountToDeposit
 
-    elif action == "w":
+elif action == "w":
         print("withdrawer")
         amountWithdraw = input("How much do you want to withdraw")
         if amountWithdraw < 0:
@@ -47,12 +47,11 @@ while true:
         else:
             if amountWithdraw > accountBalance:
                 print("you cant withdraw above your account balance")
-        else:
-            accountBalance -= amountWithdraw # accountBalance = accounrBalance - amountWithdraw
-            print("the new account balance is: ", accounrBalance)
-
-    else:
-        break
+            else:
+                accountBalance -= amountWithdraw # accountBalance = accounrBalance - amountWithdraw
+            print("the new account balance is: ", accountBalance)
+else:
+    break
     print("done")
 
 
